ec995cbfb03f5b4bd5a86a79cd8b2745864c2181
aleguzman25 <guzmanalejandra41@gmail.com>
2024-10-17 19:17:24 -0300

    .

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,21 +6,26 @@ const HomePage = () => {
             <section 
             
              id="perros" className="container my-5">
+                <img src='seccion perros' width="40%" height="60%"></img>
                 <h2>Sección de Perros</h2>
                 <p>Encuentra todo lo que necesitas para tus perros, desde comida hasta accesorios.</p>
             </section>
 
             <section id="gatos" className="container my-5">
+
+                <img src='seccion aves' width="40%" height="60%"></img>
                 <h2>Sección de Gatos</h2>
                 <p>Explora nuestra variedad de productos para gatos, incluyendo juguetes y camas.</p>
             </section>
 
             <section id="peces" className="container my-5">
+                <img src='seccion peces' width="40%" height="60%"></img>
                 <h2>Sección de Peces</h2>
                 <p>Ofrecemos acuarios, alimentos y otros artículos esenciales para peces.</p>
             </section>
 
             <section id="aves" className="container my-5">
+                <img src='seccion aves' width="40%" height="60%"></img>
                 <h2>Sección de Aves</h2>
                 <p>Encuentra jaulas, alimentos y otros productos para aves en nuestra tienda.</p>
             </section>
